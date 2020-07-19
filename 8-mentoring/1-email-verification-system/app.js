a028a6b33f0cedd7cf4cfa4a5c1a36f32f55b7fe
rebecca-dci <arowosokirebecca@gmail.com>
2020-07-19 14:06:48 +0200

    some changes

---
+++ 8-mentoring/1-email-verification-system/app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require ('./modules/mongoose')
+const app = express()
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.set('view engine', 'ejs')
+//app.set('views','./views')
+app.set('views', path.join(__dirname,'views'))
+
+
+//This link us to a page with 'Hello world'
+app.get('/', (req,res) => {
+    res.send('Hello world')
+})
+// This link us to the register page
+app.get('/register', (req,res) => {
+    res.render('register')
+})
+
+app.post('/register', (req, res) => {
+    console.log(req.body)
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const password = req.body.password
+    const email = req.body.email
+
+    if(firstName.trim() && lastName.trim() && password && email. trim()){
+        database.register(firstName,lastName,password,email).then(() => {
+            res.json(1)
+        }).catch(error => {
+            res.json(2)
+        })
+    } else {
+        res.json(2)
+    }
+})
+
+app.listen(4000, () => {
+    console.log('App listening on port 4000!')
+});
+
+
---
+++ 8-mentoring/1-email-verification-system/modules/mongoose.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose')
+const passwordHash = require('password-hash')
+
+const connectionString = 'mongodb+srv://register_mongoose:1234abc@cluster0.lh6tv.mongodb.net/register_mongoose?retryWrites=true&w=majority'
+
+
+const usersSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        max: 50,
+        min: 4
+    },
+    lastName: {
+        type: String,
+        required: true,
+        max: 50,
+        min: 4
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 100,
+        min: 4
+
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        max: 100,
+        min: 4
+    },
+    verified: {
+        type: Boolean,
+        required: true
+    }
+})
+
+
+const Users = mongoose.model('users', usersSchema)
+function connect(){
+    return new Promise((resolve, reject) => {
+        if(mongoose.connection.readyState === 1){
+            resolve()
+        }else {
+            mongoose.connect(connectionString, {
+                useCreateIndex: true,
+                useUnifiedTopology: true,
+                useNewUrlParser: true
+            }).then(() => {
+                resolve()
+            }).catch(error => {
+                reject(error)
+            })
+        }
+    })
+}
+function register (firstName, lastName, password,email){
+    return new Promise ((resolve, reject) => {
+        connect().then(() => {
+            // creating  new users
+            const newUser = new Users({
+                firstName: firstName,
+                lastName: lastName,
+                password: passwordHash.generate(password),
+                email: email
+            }) 
+            //Saving the new user in the database
+            newUser.save().then(() => {
+                resolve()
+            }).catch(error => {
+                reject(error)
+            })
+        }).catch(error => {
+            reject(error)
+        })
+    })
+}
+
+
+module.exports = {
+    register
+}
